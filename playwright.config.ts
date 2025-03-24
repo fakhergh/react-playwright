3f43f8f3e6e50b09f8cd75cd3e3060acbfe47baf
Fakher Ghouili <fakherghouili@gmail.com>
2025-03-24 15:22:52 +0400

    Build project before running tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run preview',
+    command: 'npm run build && npm run preview',
     port: 4173,
     reuseExistingServer: !process.env.CI,
     timeout: 180000,
